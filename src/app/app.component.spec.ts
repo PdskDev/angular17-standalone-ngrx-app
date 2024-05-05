70949a1c36cdad081767e183613b615a846747c8
NadetDev <nadetdev@gmail.com>
2024-05-05 14:58:28 +0200

    Add Home + Customer component + clean code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'app-standalone' title`, () => {
+  /* it(`should have the 'app-standalone' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-standalone');
-  });
+    expect(app.welcomeMessage).not.toEqual('AppStandalone');
+  }); */
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
+    const app = fixture.componentInstance;
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-standalone');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      app.welcomeMessage
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'app-standalone';
+  welcomeMessage = 'NadetDev Angular 17 Standalone NgRX app';
 }
