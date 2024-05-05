7f6c6a6595e5e2730da7c4ec6b0c075adf3c519e
NadetDev <nadetdev@gmail.com>
2024-05-05 15:24:51 +0200

    enable Customer component lazyloading + add Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { CustomerComponent } from './components/customer/customer.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'customer', component: CustomerComponent },
+  {
+    path: 'customer',
+    loadComponent: () =>
+      import('./components/customer/customer.component').then(
+        (m) => m.CustomerComponent
+      ),
+  },
 ];
