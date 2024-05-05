342b706fc96ad88f19914b124733e97da39871cc
NadetDev <nadetdev@gmail.com>
2024-05-05 15:35:54 +0200

    Export Material Module

--- src/app/components/menuheader/menuheader.component.ts
+++ src/app/components/menuheader/menuheader.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AppMaterialModule } from '../../../_module/material.module';
 
 @Component({
   selector: 'app-menuheader',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AppMaterialModule],
   templateUrl: './menuheader.component.html',
   styleUrl: './menuheader.component.css',
 })
